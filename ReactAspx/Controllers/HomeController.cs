5609fb64ad9580f4961b7e2d15e5105a08642223
LSUjess <LSUjess@gmail.com>
2020-05-05 17:10:52 -0500

    Add project files.

---
+++ ReactAspx/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using ReactAspx.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ReactAspx.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            // to create the database the first time, uncomment these 2 lines
+          //  AppDbContext c = new AppDbContext();
+          //  c.Database.CreateIfNotExists();
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
